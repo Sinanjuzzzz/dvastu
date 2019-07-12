01fed7c0d5eebae1249df7bce2600817e96af5c0
Sinanjuzzzz <yaojunjie@ewell.cc>
2019-07-12 14:55:36 +0800

    添加Layout

---
+++ src/layouts/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Menu, Icon } from 'antd';
+import Link from 'umi/link';
+import withRouter from 'umi/withRouter';
+
+function Header({ location }) {
+  return (
+    <Menu selectedKeys={[location.pathname]} mode="horizontal" theme="dark">
+      <Menu.Item key="/users">
+        <Link to="/users">
+          <Icon type="bars" />Users
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="/list">
+        <Link to="/list">
+          <Icon type="ordered-list" />List
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="/count">
+        <Link to="/count">
+          <Icon type="number" />Count
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="/deletelist">
+        <Link to="/deletelist">
+          <Icon type="compass" />快速上手demo
+        </Link>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default withRouter(Header);
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,10 @@
 import styles from './index.css';
+import Header from './Header';
 
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
+      <Header location={props.location} />
       <h1 className={styles.title}>Yay! Welcome to umi!</h1>
       {props.children}
     </div>
