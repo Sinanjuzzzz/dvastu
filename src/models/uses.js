e61e0d09dc5821f0f6943e4d0089a6a547f81e92
Sinanjuzzzz <yaojunjie@ewell.cc>
2019-07-22 15:58:34 +0800

    优化

--- src/models/uses.js
+++ src/models/uses.js
@@ -1,4 +1,4 @@
-import * as usersServices from '../services/users'
+import * as usersServices from '@/services/users'
 
 export default {
     namespace: 'users',
@@ -8,13 +8,13 @@ export default {
         page: null,
         size: null,
     },
-    reducers:{
-        save(state, { payload: { data: list, total, page, size } }){
+    reducers: {
+        save(state, { payload: { data: list, total, page, size } }) {
             return { ...state, list, total, page, size };
         },
     },
-    effects:{
-        *fetchUsersList({ payload: { page, size } }, { call, put }){
+    effects: {
+        *fetchUsersList({ payload: { page, size } }, { call, put }) {
             const { data, headers } = yield call(usersServices.queryUsersList, { page, size })
             yield put({
                 type: 'save',
@@ -26,7 +26,7 @@ export default {
                 }
             })
         },
-        *queryUser({ payload: { queryMode, queryValue } }, { call, put }){
+        *queryUser({ payload: { queryMode, queryValue } }, { call, put }) {
             const { data } = yield call(usersServices.queryUser, { queryMode, queryValue })
             yield put({
                 type: 'save',
