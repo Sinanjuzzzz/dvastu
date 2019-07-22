9b3741cf5bdabd3b3341351893eb73414884380d
Sinanjuzzzz <yaojunjie@ewell.cc>
2019-07-22 16:28:31 +0800

    按用户查看todos

--- src/pages/todo/index.js
+++ src/pages/todo/index.js
@@ -4,25 +4,36 @@ import { Icon, Descriptions, Row, Col, Collapse } from 'antd';
 const { Panel } = Collapse;
 
 const mapStatetoProps = (state) => {
-  const { list, total, page, size } = state.todos;
+  const { list: todosList, total, page, size } = state.todos;
+  const { list: usersList, total: usersTotal } = state.users;
   return {
-    list, total, page, size,
+    todosList, usersList, total, usersTotal, page, size,
   };
 }
 
 @connect(mapStatetoProps)
 class ToDoCards extends React.Component {
-
   constructor(props) {
     super(props);
-    this.fetchToDosList(1, 3)
-
+    this.fetchUsersList(1, this.props.usersTotal)
+    this.fetchToDosList(1,3)
   }
 
   fetchToDosList = (page, size) => {
     const { dispatch } = this.props;
     dispatch({
       type: 'todos/fetchToDosList',
+      payload: {
+        page,
+        size,
+      }
+    })
+  }
+
+  fetchUsersList = (page, size) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'users/fetchUsersList',
       payload: {
         page,
         size,
@@ -42,28 +53,32 @@ class ToDoCards extends React.Component {
   }
 
   render() {
-    const { list } = this.props
+    const { usersList, todosList } = this.props
     return (
       <Row type="flex" justify="center" style={{ textAlign: "left" }}>
         <Col span={20}>
-          <Collapse>
-            <Panel key={1}>
+          <Collapse accordion onChange={activeKey=>this.queryToDo('userId', activeKey)}>
+          {
+            usersList.length?(usersList.map(userItem=>
+              <Panel key={userItem.id} header={userItem.name}>
               <Collapse defaultActiveKey="0">
                 {
-                  list.length ? (list.map(item =>
-                    <Panel header={"id: "+item.id}>
+                  todosList.length ? (todosList.map(todoitem =>
+                    <Panel header={"id: " + todoitem.id} key={todoitem.id}>
                       <Descriptions bordered>
-                        <Descriptions.Item label="ID">{item.id}</Descriptions.Item>
-                        <Descriptions.Item label="UserID">{item.userId}</Descriptions.Item>
-                        <Descriptions.Item label="Title">{item.title}</Descriptions.Item>
-                        <Descriptions.Item label="completed">{item.completed ? <Icon type="check" /> : <Icon type="close" />}</Descriptions.Item>
+                        <Descriptions.Item label="ID">{todoitem.id}</Descriptions.Item>
+                        <Descriptions.Item label="UserID">{todoitem.userId}</Descriptions.Item>
+                        <Descriptions.Item label="Title">{todoitem.title}</Descriptions.Item>
+                        <Descriptions.Item label="completed">{todoitem.completed ? <Icon type="check" /> : <Icon type="close" />}</Descriptions.Item>
                       </Descriptions>
                     </Panel>
                   )) : null
                 }
               </Collapse>
-
             </Panel>
+              )):null
+          }
+            
           </Collapse>
         </Col>
       </Row>
