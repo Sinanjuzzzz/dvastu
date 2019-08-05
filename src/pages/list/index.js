988ce0dc7d136d8dffc36d4832dbab81a291969a
Sinanjuzzzz <yaojunjie@ewell.cc>
2019-08-05 15:06:13 +0800

    使用useState

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,59 +1,46 @@
-import React from 'react';
-import { connect } from 'dva';
+import React, { useState } from 'react';
 
-const mapStateToProps = (state) => {
-	const { list1, list2 } = state.list;
-	return {
-		list1,
-		list2,
-	};
+function List() {
+
+	const [list1, setList1] = useState(['1', '2', '3'])
+	const [list2, setList2] = useState(['4', '5', '6'])
+
+	function movetoList1(value) {
+		let newlist = list2
+		newlist.splice(list2.indexOf(value), 1)
+		setList2(newlist)
+		setList1(list1.concat([value]))
+	}
+
+	function movetoList2(value) {
+		let newlist = list1
+		newlist.splice(list1.indexOf(value), 1)
+		setList1(newlist)
+		setList2(list2.concat([value]))
+	}
+
+	return (
+		<div>
+			<h3>List1</h3>
+			{
+				list1 instanceof Array ? list1.map((item) => (
+					<div key={item} id={item} onClick={(e) => { movetoList2(item); }} >
+						{item}
+					</div>
+				)
+				) : null
+			}
+			<h3>list2</h3>
+			{
+				list2 instanceof Array ? list2.map((item) => (
+					<div key={item} id={item} onClick={(e) => { movetoList1(item); }} >
+						{item}
+					</div>
+				)
+				) : null
+			}
+		</div>
+	);
 }
-@connect(mapStateToProps)
-class List extends React.Component {
-
-	list1pop = (popItem) => {
-		const { dispatch } = this.props
-		dispatch({
-			type: 'list/list1pop',
-			payload: { popItem },
-		})
-	}
-
-	list2pop = (popItem) => {
-		const { dispatch } = this.props
-		dispatch({
-			type: 'list/list2pop',
-			payload: { popItem },
-		})
-	}
-	render() {
-		const { list1, list2 } = this.props;
-		return (
-			<div>
-				<h3>List1</h3>
-				{
-					list1.map((item, key) => {
-						return (
-							<div key={item} id={item} onClick={(e) => { this.list1pop(e.target.id); this.forceUpdate() }} >
-								{item}
-							</div>
-						)
-					})
-				}
-				<h3>list2</h3>
-				{
-					list2.map((item, key) => {
-						return (
-							<div key={item} id={item} onClick={(e) => { this.list2pop(e.target.id); this.forceUpdate() }} >
-								{item}
-							</div>
-						)
-					})
-				}
-			</div>
-		);
-	}
-
-};
 
 export default List
