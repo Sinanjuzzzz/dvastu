b8bdd0aa1293b0cc7565be80914e4ff7bf01266b
Sinanjuzzzz <yaojunjie@ewell.cc>
2019-08-05 10:03:54 +0800

    使用useEffect

--- src/pages/todos/index.js
+++ src/pages/todos/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'dva'
 import { Icon, Descriptions, Row, Col, Collapse, Spin } from 'antd';
 const { Panel } = Collapse;
@@ -17,26 +17,23 @@ const mapStatetoProps = ({ todos, users, loading }) => {
   };
 }
 
-@connect(mapStatetoProps)
-class ToDoCards extends React.Component {
-  constructor(props) {
-    super(props);
-    this.fetchUsersList(1, this.props.usersTotal)
-  }
+function ToDoCards(props) {
+  const { usersTotal, usersList, todosList, todosLoading, dispatch } = props
 
-  fetchToDosList = (page, size) => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'todos/fetchToDosList',
-      payload: {
-        page,
-        size,
-      }
-    })
-  }
+  useEffect(() => { fetchUsersList(1, usersTotal) }, [fetchUsersList, usersTotal])
 
-  fetchUsersList = (page, size) => {
-    const { dispatch } = this.props;
+  // function fetchToDosList(page, size) {
+  //   const { dispatch } = this.props;
+  //   dispatch({
+  //     type: 'todos/fetchToDosList',
+  //     payload: {
+  //       page,
+  //       size,
+  //     }
+  //   })
+  // }
+
+  function fetchUsersList(page, size) {
     dispatch({
       type: 'users/fetchUsersList',
       payload: {
@@ -46,8 +43,7 @@ class ToDoCards extends React.Component {
     })
   }
 
-  queryToDo = (queryMode, queryValue) => {
-    const { dispatch } = this.props;
+  function queryToDo(queryMode, queryValue) {
     dispatch({
       type: 'todos/queryToDo',
       payload: {
@@ -57,40 +53,36 @@ class ToDoCards extends React.Component {
     })
   }
 
-  render() {
-    const { usersList, todosList, todosLoading } = this.props
-    return (
-      <Row type="flex" justify="center" style={{ textAlign: "left" }}>
-        <Col span={20}>
-          <Collapse accordion onChange={activeKey => this.queryToDo('userId', activeKey)}>
-            {
-              usersList.length ? (usersList.map(userItem =>
-                <Panel key={userItem.id} header={userItem.name}>
-                  <Spin spinning={todosLoading}>
-                    <Collapse defaultActiveKey="0">
-                      {
-                        todosList.length ? (todosList.map(todoitem =>
-                          <Panel header={"id: " + todoitem.id} key={todoitem.id}>
-                            <Descriptions bordered>
-                              <Descriptions.Item label="ID">{todoitem.id}</Descriptions.Item>
-                              <Descriptions.Item label="UserID">{todoitem.userId}</Descriptions.Item>
-                              <Descriptions.Item label="Title">{todoitem.title}</Descriptions.Item>
-                              <Descriptions.Item label="completed">{todoitem.completed ? <Icon type="check" /> : <Icon type="close" />}</Descriptions.Item>
-                            </Descriptions>
-                          </Panel>
-                        )) : null
-                      }
-                    </Collapse>
-                  </Spin>
-                </Panel>
-              )) : null
-            }
-
-          </Collapse>
-        </Col>
-      </Row>
-    )
-  }
+  return (
+    <Row type="flex" justify="center" style={{ textAlign: "left" }}>
+      <Col span={20}>
+        <Collapse accordion onChange={activeKey => queryToDo('userId', activeKey)}>
+          {
+            usersList.length ? (usersList.map(userItem =>
+              <Panel key={userItem.id} header={userItem.name}>
+                <Spin spinning={todosLoading}>
+                  <Collapse defaultActiveKey="0">
+                    {
+                      todosList.length ? (todosList.map(todoitem =>
+                        <Panel header={"id: " + todoitem.id} key={todoitem.id}>
+                          <Descriptions bordered>
+                            <Descriptions.Item label="ID">{todoitem.id}</Descriptions.Item>
+                            <Descriptions.Item label="UserID">{todoitem.userId}</Descriptions.Item>
+                            <Descriptions.Item label="Title">{todoitem.title}</Descriptions.Item>
+                            <Descriptions.Item label="completed">{todoitem.completed ? <Icon type="check" /> : <Icon type="close" />}</Descriptions.Item>
+                          </Descriptions>
+                        </Panel>
+                      )) : null
+                    }
+                  </Collapse>
+                </Spin>
+              </Panel>
+            )) : null
+          }
+        </Collapse>
+      </Col>
+    </Row>
+  )
 }
 
-export default ToDoCards
+export default connect(mapStatetoProps)(ToDoCards)
